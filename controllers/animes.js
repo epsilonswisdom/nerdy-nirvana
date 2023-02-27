0a0fb1f778c21d5933ee8a9c300fb4b30a6c00bb
Manny <emaldonado1402@gmail.com>
2023-02-27 04:18:23 -0500

    work on the controller function for Anime from the model

---
+++ controllers/animes.js
@@ -0,0 +1,17 @@
+const { Anime, Profile } = require('../models')
+
+async function create(req, res) {
+  try {
+    req.body.profileId = req.user.profile.id
+    const anime = await Anime.create(req.body)
+    res.status(200).json(anime)
+  } catch (error) {
+    res.status(500).json({err: error})
+  }
+}
+
+async function index(req, res) {
+  try {
+    const animes = await Anime.findAll({})
+  }
+}
