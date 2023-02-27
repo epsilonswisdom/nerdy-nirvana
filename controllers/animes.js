bf9c9a0115eef102bf9b6efd033bdd5763718331
Manny <emaldonado1402@gmail.com>
2023-02-27 05:48:32 -0500

    add the route for comment

--- controllers/animes.js
+++ controllers/animes.js
@@ -1,5 +1,5 @@
 const { Anime, Profile, Comment } = require('../models')
-
+const comment = require('../models/comment')
 async function create(req, res) {
   try {
     req.body.profileId = req.user.profile.id
@@ -12,9 +12,22 @@ async function create(req, res) {
 
 async function index(req, res) {
   try {
-    const animes = await Anime.findAll({})
+    const animes = await Anime.findAll({
+      include: [{ model: Comment, as: 'comments'}]
+    })
   
     res.status(200).json(animes)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+async function addComment(req, res) {
+  try {
+    req.body.animeId = req.params.id
+    req.body.profileId = req.user.profile.id
+    const comment = await Comment.create(req.body)
+    res.status(200).json(comment)
   } catch (error) {
     res.status(500).json(error)
   }
@@ -23,4 +36,5 @@ async function index(req, res) {
 module.exports = {
   create,
   index,
+  addComment,
 }
