58cb6436abb589578f45fc9e7030ee6007824d3c
Manny <emaldonado1402@gmail.com>
2023-02-27 04:38:32 -0500

    Finish the last function for Votes Delete Function

---
+++ routes/animes.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+const animesCtrl = require('../controllers/animes.js')
+const middleware = require('../middleware/auth.js')
+
+const { decodeUserFromToken, checkAuth } = middleware
+
+/*----------Public Routes --------*/
+router.get('/', animesCtrl.index)
+
+
+/*--------Protected Routes ------*/
+router.use(decodeUserFromToken)
+router.post('/', checkAuth, animesCtrl.create)
+
+module.exports = router
